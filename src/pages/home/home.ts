0444c1350ae757b9bbc64e5565760bb3869e625e
Jeremy Cannell <cannell001@gannon.edu>
2016-11-01 21:14:17 -0400

    accessed reote data to populate the list

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-
+import { Http } from '@angular/http';
 import { NavController } from 'ionic-angular';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -8,27 +9,19 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  items = [
-    {title: 'Apple'},
-    {title: 'Cherry'},
-    {title: 'Banana'},
-    {title: 'Other'},
-    {title: 'Tomato'},
-    {title: 'Grape'},
-    {title: 'Pomegranate'},
-    {title: 'Carrot'},
-    {title: 'Apple'},
-    {title: 'Cherry'},
-    {title: 'Banana'},
-    {title: 'Other'},
-    {title: 'Tomato'},
-    {title: 'Grape'},
-    {title: 'Pomegranate'},
-    {title: 'Carrot'},
-  ]
+  items
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private http: Http) {
+        //this.http.get("http://task.lafrim.net/artists/api.php/artists").map(res => res.json()).subscribe(data => {
+        //this.http.get("https://api.randomuser.me/?results=10").map(res => res.json()).subscribe(data => {
+        this.http.get("http://localhost:3000/db").map(res => res.json()).subscribe(data => {
+        this.items = data;
+        //this.items.push(data);
+        console.log("GOT RESULTS");
+        },
+        error => {
+          console.log("OOPS!");
+        });
   }
 
   itemClicked(event, item) {
